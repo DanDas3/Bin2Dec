70d617ec2561308145c91adb0a05a5d639e5438a
Danilo Araújo <daniloaraujo1990@hotmail.com>
2022-08-12 16:12:47 -0300

    Progress
    Implementing the conversion and added bootstrap in project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Bin2Dec';
+  form:FormGroup;
+  decimalValue: Number = 0;
+
+  constructor(public fb:FormBuilder) {
+    this.form = this.fb.group({
+      binaryNumber:[''],
+      decimalNumber:['']
+    });
+  }
+
+  ngOnInit() {
+
+  }
+
+  onlyBinary(evento:any) {
+    return (evento.charCode >= 48 && evento.charCode <= 49);
+
+  }
+
+  convert(binValue:string) {
+    let decimalValue = 0;
+    decimalValue = this.conversionBinToDec(binValue, decimalValue);
+
+    this.form.patchValue(
+    {
+      decimalNumber: decimalValue
+    });
+  }
+
+  private conversionBinToDec(binValue: string, decimalValue: number) {
+    for (let i = 0, j = binValue.length - 1; i < binValue.length && j >= 0; ++i, --j) {
+      decimalValue += Number.parseInt(binValue[j]) * 2 ** i;
+    }
+    return decimalValue;
+  }
 }
